3d020430c83e11fbcdfb0630e068f1a15bad743d
Jonthan Darko Adoo <adoojonathan412@gmail.com>
2024-09-10 17:50:07 +0000

    build navabr component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,9 @@
-
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-white" >Netflix clone</h1>
+      <Navbar />
     </>
   );
 }
